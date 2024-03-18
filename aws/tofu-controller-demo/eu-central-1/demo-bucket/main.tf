70558c4ae2566f1a8b7a63b5747a9dea091da76b
joerx-tw <71110905+joerx-tw@users.noreply.github.com>
2024-03-18 23:33:18 +0100

    Change Terraform version

--- aws/tofu-controller-demo/eu-central-1/demo-bucket/main.tf
+++ aws/tofu-controller-demo/eu-central-1/demo-bucket/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "~> 1.6.0"
+  required_version = "~> 1.3.9" # Only version supported by TF controller
 
   backend "s3" {
     bucket         = "terraform-upx-tofu-controller-demo-zkk"
